9caa3cf446709e073812a85018862ef431c60ee6
darklooters11 <145337741+darklooters11@users.noreply.github.com>
2023-09-20 16:58:59 +0530

    Update AccountsService.java

--- src/main/java/com/dws/challenge/service/AccountsService.java
+++ src/main/java/com/dws/challenge/service/AccountsService.java
@@ -3,17 +3,23 @@ package com.dws.challenge.service;
 import com.dws.challenge.domain.Account;
 import com.dws.challenge.exception.DuplicateAccountIdException;
 import com.dws.challenge.repository.AccountsRepository;
-import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
+@Slf4j
 public class AccountsService {
 
     private final AccountsRepository accountsRepository;
     private final NotificationService notificationService;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     @Autowired
     public AccountsService(AccountsRepository accountsRepository, NotificationService notificationService) {
@@ -21,42 +27,85 @@ public class AccountsService {
         this.notificationService = notificationService;
     }
 
+    @Transactional
     public void createAccount(Account account) {
         this.accountsRepository.createAccount(account);
     }
 
+    @Transactional(readOnly = true)
     public Account getAccount(String accountId) {
         return this.accountsRepository.getAccount(accountId);
     }
 
-    public synchronized String transferMoney(String accountFromId, String accountToId, BigDecimal amount) {
-        Account accountFrom = accountsRepository.getAccount(accountFromId);
-        Account accountTo = accountsRepository.getAccount(accountToId);
-
-        if (accountFrom == null || accountTo == null) {
-            throw new IllegalArgumentException("One or both accounts do not exist.");
-        }
-
+    public String transferMoney(String accountFromId, String accountToId, BigDecimal amount) {
+        // Check if the transfer amount is valid
         if (amount.compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("Amount to transfer must be a positive number.");
         }
 
-        if (accountFrom.getBalance().compareTo(amount) < 0) {
-            throw new IllegalArgumentException("Insufficient funds in the source account.");
+        Account accountFrom = accountsRepository.getAccount(accountFromId);
+        Account accountTo = accountsRepository.getAccount(accountToId);
+
+        // Check if accounts exist
+        if (accountFrom == null || accountTo == null) {
+            return "One or both accounts do not exist.";
         }
 
-        // Deduct the amount from the source account
-        accountFrom.setBalance(accountFrom.getBalance().subtract(amount));
+        // Use an asynchronous executor to perform the transfer asynchronously
+        executorService.submit(() -> {
+            performMoneyTransfer(accountFromId, accountToId, amount);
+        });
 
-        // Add the amount to the target account
-        accountTo.setBalance(accountTo.getBalance().add(amount));
-        // Notify both account holders
-        String notificationFrom = "Transferred $" + amount + " to Account " + accountToId;
-        String notificationTo = "Received $" + amount + " from Account " + accountFromId;
+        return "Transfer initiated";
+    }
 
-        // Use the notification service to notify account holders
-        notificationService.notifyAboutTransfer(accountFrom, notificationFrom);
-        notificationService.notifyAboutTransfer(accountTo, notificationTo);
-        return notificationFrom;
+
+    @Transactional
+    public void performMoneyTransfer(String accountFromId, String accountToId, BigDecimal amount) {
+        Account accountFrom = accountsRepository.getAccount(accountFromId);
+        Account accountTo = accountsRepository.getAccount(accountToId);
+
+        // Check if accounts exist
+        if (accountFrom == null || accountTo == null) {
+            throw new IllegalArgumentException("One or both accounts do not exist.");
+        }
+
+        // Check if the transfer amount is valid
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Amount to transfer must be a positive number.");
+        }
+
+        // Determine a consistent order for locking accounts
+        String firstAccountId = accountFromId.compareTo(accountToId) < 0 ? accountFromId : accountToId;
+        String secondAccountId = accountFromId.compareTo(accountToId) < 0 ? accountToId : accountFromId;
+
+        // Lock the first account
+        synchronized (firstAccountId.intern()) {
+            // Lock the second account
+            synchronized (secondAccountId.intern()) {
+                // Check if the source account has sufficient funds
+                if (accountFrom.getBalance().compareTo(amount) < 0) {
+                    throw new IllegalArgumentException("Insufficient funds in the source account.");
+                }
+
+                // Deduct the amount from the source account
+                accountFrom.setBalance(accountFrom.getBalance().subtract(amount));
+
+                // Add the amount to the target account
+                accountTo.setBalance(accountTo.getBalance().add(amount));
+
+                // Notify both account holders asynchronously
+                String notificationFrom = "Transferred $" + amount + " to Account " + accountToId;
+                String notificationTo = "Received $" + amount + " from Account " + accountFromId;
+
+                notifyAccountsAsync(accountFrom, notificationFrom);
+                notifyAccountsAsync(accountTo, notificationTo);
+            }
+        }
+    }
+
+    @Async
+    public void notifyAccountsAsync(Account account, String notification) {
+        notificationService.notifyAboutTransfer(account, notification);
     }
 }
